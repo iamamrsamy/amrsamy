2e89e3278c4f021ee76407e263a5b6f99bf7e564
iamamrsamy <amrsamydxb@gmail.com>
2026-08-28 16:48:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,119 +1,37 @@
-/* =========================
-   QR CODE
-========================= */
-
-const currentURL = window.location.href;
-
-
-QRCode.toCanvas(
-
-    document.getElementById("qrcode"),
-
-    currentURL,
-
-    {
-
-        width: 220,
-
-        margin: 2,
-
-        color: {
-
-            dark: "#050505",
-
-            light: "#ffffff"
-
-        }
-
-    },
-
-    function(error) {
-
-        if (error) {
-
-            console.error(error);
-
-        }
-
-    }
-
-);
-
-
-
-/* =========================
-   SAVE CONTACT
-========================= */
-
 function saveContact() {
 
-    const vcard = `BEGIN:VCARD
+    const vCard = `BEGIN:VCARD
 VERSION:3.0
 FN:Amr Samy
 N:Samy;Amr;;;
-TITLE:The Magician
+ORG:Amr Samy
+TITLE:The Happiness Maker
 TEL;TYPE=CELL:+201115552621
 EMAIL:amrsamydxb@gmail.com
-URL:${window.location.href}
-NOTE:Magician - Entertainer - Illusionist
+URL:https://iamamrsamy.github.io/amrsamy/
+NOTE:Amr Samy - The Happiness Maker
 END:VCARD`;
 
-
-    const file = new Blob(
-
-        [vcard],
-
+    const blob = new Blob(
+        [vCard],
         {
-            type: "text/vcard"
+            type: "text/vcard;charset=utf-8"
         }
-
     );
 
+    const url = URL.createObjectURL(blob);
 
-    const link =
-        document.createElement("a");
+    const link = document.createElement("a");
 
+    link.href = url;
 
-    link.href =
-        URL.createObjectURL(file);
-
-
-    link.download =
-        "Amr-Samy.vcf";
-
+    link.download = "Amr-Samy.vcf";
 
     document.body.appendChild(link);
 
-
     link.click();
-
 
     document.body.removeChild(link);
 
-
-    showMessage();
-
-}
-
-
-
-/* =========================
-   MESSAGE
-========================= */
-
-function showMessage() {
-
-    const message =
-        document.getElementById("message");
-
-
-    message.classList.add("show");
-
-
-    setTimeout(function() {
-
-        message.classList.remove("show");
-
-    }, 2000);
-
+    URL.revokeObjectURL(url);
 }
